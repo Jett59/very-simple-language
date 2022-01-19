899ef3fd1bb1a65287548d48c4fb8fb41213f2b5
Jett Thompson <jett.thompson.59@gmail.com>
2022-01-20 10:41:46 +1100

    improved the grammar and added macro parameters

--- src/main/java/app/cleancode/parser/NodeType.java
+++ src/main/java/app/cleancode/parser/NodeType.java
@@ -2,11 +2,12 @@ package app.cleancode.parser;
 
 public enum NodeType {
     // Terminal symbols
-    IDENTIFIER(true), INTEGER(true), EQUALS(true), SEMICOLON(true), COMMA(true), FUNCTION_SPECIFIER(
-            true), LEFT_PAREN(true), RIGHT_PAREN(true), LEFT_BRACE(true), RIGHT_BRACE(true),
+    IDENTIFIER(true), INTEGER(true), EQUALS(true), SEMICOLON(true), COLON(true), COMMA(
+            true), MACRO_SPECIFIER(
+                    true), LEFT_PAREN(true), RIGHT_PAREN(true), LEFT_BRACE(true), RIGHT_BRACE(true),
     // Non terminal symbols
-    PROGRAM(false), PROGRAM_ELEMENT_LIST(false), PROGRAM_ELEMENT(false), FUNCTION_DEFINITION(
-            false), FUNCTION_DECLARATION(false);
+    PROGRAM(false), PROGRAM_ELEMENT_LIST(false), PROGRAM_ELEMENT(false), MACRO_DEFINITION(
+            false), PARAMETER_LIST(false);
 
     public final boolean terminal;
 
