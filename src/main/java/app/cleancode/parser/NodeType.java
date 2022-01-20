4bf834a517f1b3470fea18059889db950720cb95
Jett Thompson <jett.thompson.59@gmail.com>
2022-01-20 16:11:33 +1100

    added support for rules on the root scope and for macro invokations

--- src/main/java/app/cleancode/parser/NodeType.java
+++ src/main/java/app/cleancode/parser/NodeType.java
@@ -2,13 +2,13 @@ package app.cleancode.parser;
 
 public enum NodeType {
     // Terminal symbols
-    IDENTIFIER(true), INTEGER(true), EQUALS(true), SEMICOLON(true), COLON(true), COMMA(
-            true), MACRO_SPECIFIER(
+    IDENTIFIER(true), INTEGER(true), STRING(true), EQUALS(true), SEMICOLON(true), COLON(
+            true), COMMA(true), MACRO_SPECIFIER(
                     true), LEFT_PAREN(true), RIGHT_PAREN(true), LEFT_BRACE(true), RIGHT_BRACE(true),
     // Non terminal symbols
     PROGRAM(false), PROGRAM_ELEMENT_LIST(false), PROGRAM_ELEMENT(false), MACRO_DEFINITION(
-            false), PARAMETER_LIST(
-                    false), RULE_LIST(false), RULE(false), SYMBOL_LIST(false), SYMBOL(false);
+            false), PARAMETER_LIST(false), RULE_LIST(
+                    false), RULE(false), MACRO_INVOKATION(false), SYMBOL_LIST(false), SYMBOL(false);
 
     public final boolean terminal;
 
